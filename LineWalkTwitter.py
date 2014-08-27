c704bb47eff2646cef06c51fe0f5362bb5af0933
Darcy R <geodarcy@gmail.com>
2014-08-27 10:10:42 -0600

    Mapping Line's tweets from El Camino 2014
    Using Python to collect Line's geotagged tweets while she walks El
    Camino 2014. Using Leaflet to map tweets.

---
+++ LineWalkTwitter.py
@@ -0,0 +1,46 @@
+import twitter
+import json
+from geojson import Feature, Point, FeatureCollection
+import geojson
+from dateutil import parser
+
+CONSUMER_KEY = '1rOtJNRrQ2UVLU2qmQUg'
+CONSUMER_SECRET = 'CSaZDwHeeMc2ZnVvvuEDf60IUkmlzwJRi2jqq3II0w'
+OAUTH_TOKEN = '15410243-GVm4aeHbw2UIXRRBSKhqOwklFUNUOheXBBJmFuA6z'
+OAUTH_TOKEN_SECRET = 'sIzbJaCdKbwLakuo6potfqCifXrcLCxYoTDlrJmM'
+auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+twitter_api = twitter.Twitter(auth=auth)
+## collect data as a stream
+try:
+  featureDict
+except(NameError):
+  try:
+    with open('S:/developer/Darcy/github/lineswalk/featureDict.txt', 'r') as fd:
+      featureDict = json.load(fd)
+  except:
+    featureDict = {}
+
+tweets = twitter_api.statuses.user_timeline(screen_name='lineperron')
+for tweet in tweets:
+  if tweet['coordinates']:
+    try:
+      picURL = [tweet['extended_entities']['media'][x]['media_url'] for x in range(len(tweet['extended_entities']['media']))]
+    except:
+      picURL = None
+    featureDict[str(tweet['id'])] = Feature(geometry=Point((tweet['coordinates']['coordinates'])), properties={'user': tweet['user']['name'], 'tweet': tweet['text'], 'date': parser.parse(tweet['created_at']).strftime("%a, %d %b %Y at %H:%M"), 'picture': picURL})
+
+badIDs = ['466216835143069697', '466635406876762112', '466641320380227585', '466649563944992768', '466650889672228864', '477524611391696897']
+for i in badIDs:
+  try:
+    del featureDict[i]
+  except:
+    pass
+
+print("Tweets collected: {}".format(len(featureDict.keys())))
+
+myFeatureCollection = FeatureCollection([x for x in featureDict.values()])
+with open('S:/developer/Darcy/github/lineswalk/twitter.json', 'w') as fp:
+  geojson.dump(myFeatureCollection, fp)
+
+with open('S:/developer/Darcy/github/lineswalk/featureDict.txt', 'w') as fd:
+  json.dump(featureDict, fd)
